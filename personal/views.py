5645de04e78db2588a655cd7abadd9f9318b4696
monish lalchandani <monish.lalchandani@tarams.com>
2017-02-14 16:26:39 +0530

    second commit

--- personal/views.py
+++ personal/views.py
@@ -4,4 +4,4 @@ def index(request):
     return render(request, "personal/home.html")
 
 def contact(request):
-    return render(request, 'personal/basic.html',{'content':['If you would like to contact me, please email me.','monish.lalchandani@tarams.com']}) 
+    return render(request, 'personal/basic.html',{'content':['hello','Why the hell is this printing','If you would like to contact me, please email me.','monish.lalchandani@tarams.com']})
