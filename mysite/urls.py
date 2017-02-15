db74e808f5a309093a7a1efa551d696d043e8fd7
monish lalchandani <monish.lalchandani@tarams.com>
2017-02-15 10:13:15 +0530

    third commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^webapp/', include('webapp.urls')),
     url(r'^personal/', include('personal.urls')),
-
+    url(r'^blog/', include('blog.urls')),
 ]
